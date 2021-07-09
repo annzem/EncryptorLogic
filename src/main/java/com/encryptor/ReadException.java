6b28065cafc55e9b23828da893886a7a77897fc1
annzem <annzemlinsky@gmail.com>
2021-07-09 14:53:08 +0200

    init

---
+++ src/main/java/com/encryptor/ReadException.java
@@ -0,0 +1,15 @@
+package com.encryptor;
+
+public class ReadException extends Exception {
+    public ReadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ReadException(String message) {
+        super(message);
+    }
+
+    public ReadException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/main/java/com/encryptor/WriteException.java
@@ -0,0 +1,15 @@
+package com.encryptor;
+
+public class WriteException extends Exception {
+    public WriteException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public WriteException(String message) {
+        super(message);
+    }
+
+    public WriteException(Throwable cause) {
+        super(cause);
+    }
+}
